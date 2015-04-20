e9cf28fcab8744b8c8beec544df3c07bbe112c56
Reid Cooper <reid.cooper8@gmail.com>
2015-04-20 15:00:39 -0400

    Updated footer to dynamically change the year

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,10 @@
 <hr>
 <footer>
   <div class="col-md-12 text-center">
+    <?php
+    // If gives you a warning about not having a time zone set or not relying on server time
+    date_default_timezone_set("America/New_York");
+    ?>
     <p>&copy; James Reid Cooper <?php
         $fromYear = 2015;
         $thisYear = (int)date('Y');
